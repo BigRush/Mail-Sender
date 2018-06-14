1343ac24cd4bc7ca9faf27b1f62011aee8e112fc
BigRush <tom.harpaz98@gmail.com>
2018-06-14 15:45:17 +0300

    test

--- Mail-sender.py
+++ Mail-sender.py
@@ -7,11 +7,6 @@ import getpass
 Rec_Mail = input("Please enter the recipient's mail: ")
 Msg_Subject = input("Please enter the subject: ")
 Msg_Content = input("Please enter your message: ")
-
-Server_Mail = smptlib.SMTP("smpt.gmail.com",587)
-
-Server_Mail.starttls()
-
 User_Mail = input("Please enter your email: ")
 while not User_Name.isalnum():
     print("You must enter only alphanumeric characters, try again...")
@@ -19,6 +14,11 @@ while not User_Name.isalnum():
 
 
 Passwd_Mail = getpass.getpass("Please enter your password: ")
+Server_Mail = smptlib.SMTP("smpt.gmail.com",587)
+
+Server_Mail.starttls()
+
+
 
 Server_Mail.login(User_Mail, Passwd_Mail)
 
